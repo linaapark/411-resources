a1a81d8f2389ae2c4e2f67d8fc36a8eb7617bb21
linaapark <psuhhyun@gmail.com>
2025-02-12 14:36:33 -0500

    added the app.run to use port specified environemnets

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -19,4 +19,4 @@ if __name__ == '__main__':
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
     port = int(os.getenv('PORT', 5001))
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=port)
